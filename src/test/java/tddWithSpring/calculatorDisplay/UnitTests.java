e0f3d3462c06ebb036fee0c4abbdaaff23a4fc11
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-03-16 21:19:52 +0200

    Project Setup - Initial empty project - Application - 1 Integration test, 1 unit test

---
+++ src/test/java/tddWithSpring/calculatorDisplay/UnitTests.java
@@ -0,0 +1,15 @@
+package tddWithSpring.calculatorDisplay;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class UnitTests {
+
+	@Test
+	public void canRunUnitTests() {
+		assertTrue(true);
+	}
+
+}
